1b14a1213f82095e0e79f1477a26f742e5d53d41
vrushabhdarekar <vrushabhdarekar45@gmail.com>
2026-02-10 20:03:07 +0530

    added filter by genre

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,18 @@
 import { useGetBooksQuery } from '../features/books/booksApi'
 import BookCard from '../components/BookCard'
 import AddBookForm from '../components/AddBookForm'
+import { useState } from 'react'
+
 
 function Dashboard() {
-  const { data: books = [], isLoading, error } = useGetBooksQuery()
+  const [genre, setGenre] = useState('')
+
+  const {
+  data: books = [],
+  isLoading,
+  error,
+  } = useGetBooksQuery(genre)
+
 
   if (isLoading) {
     return <p className="text-center mt-8">Loading books...</p>
@@ -16,6 +25,22 @@ function Dashboard() {
   return (
     <div>
       <AddBookForm />
+
+        <div className="mb-6 flex items-center gap-4">
+        <label className="font-medium">Filter by Genre:</label>
+
+        <select
+            value={genre}
+            onChange={(e) => setGenre(e.target.value)}
+            className="border rounded px-3 py-2 bg-white"
+        >
+            <option value="">All</option>
+            <option value="Fiction">Fiction</option>
+            <option value="Tech">Tech</option>
+            <option value="Sci-Fi">Sci-Fi</option>
+            <option value="Biography">Biography</option>
+        </select>
+        </div>
 
       {books.length === 0 ? (
         <p className="text-center text-gray-500 mt-8">
