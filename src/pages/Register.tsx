33b65c0da5388fde86551c998cd2aff8f6533837
vrushabhdarekar <vrushabhdarekar45@gmail.com>
2026-02-27 11:22:47 +0530

    bug fix: register

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -19,7 +19,7 @@ const Register = () => {
     setIsLoading(true)
 
     try {
-      const res = await fetch('http://localhost:3000/auth/register', {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/auth/register`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ fullName, email, password }),
